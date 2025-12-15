d63216d3361004cc485431a15903eebd9f92d3ea
23120096 <viettoan110905@gmail.com>
2025-12-15 07:47:42 +0700

    config: setup tailwindcss and update npm scripts

---
+++ BTCN_2/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      maxWidth: {
+        'content': '1200px',
+      },
+    },
+  },
+  plugins: [],
+}
