f1e3172fd8c3f75c012a4b680a4c645e941f8a00
23120096 <viettoan110905@gmail.com>
2025-12-16 04:00:26 +0700

    0.5 diem: chuc nang ve Home va co Hieu ung Loading - fix mau va vi tri cua Loadingding Spinner

--- BTCN_2/src/components/ui/LoadingSpinner.jsx
+++ BTCN_2/src/components/ui/LoadingSpinner.jsx
@@ -3,7 +3,7 @@ const LoadingSpinner = () => {
     <div className="flex justify-center items-center min-h-[50vh] w-full">
       <div className="relative">
         <div className="w-12 h-12 rounded-full border-4 border-gray-700 opacity-30"></div>
-        <div className="w-12 h-12 rounded-full border-4 border-t-blue-500 border-r-transparent border-b-transparent border-l-transparent absolute inset-0 animate-spin"></div>
+        <div className="w-12 h-12 rounded-full border-4 border-t-red-500 border-r-transparent border-b-transparent border-l-transparent absolute inset-0 animate-spin"></div>
       </div>
     </div>
   );
